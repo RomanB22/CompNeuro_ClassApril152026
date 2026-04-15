4a2da182a036db7739cc7dc199a5e581b863fba1
Roman Baravalle <romanbaravalle@gmail.com>
2026-04-15 10:02:05 -0400

    updated tut1

--- tut1/tut1.py
+++ tut1/tut1.py
@@ -1,3 +1,22 @@
 import HHTut
 from netpyne import sim
 sim.createSimulateAnalyze(netParams = HHTut.netParams, simConfig = HHTut.simConfig)
+
+from pprint import pprint
+pprint(sim.specs.simConfig.__dict__)
+pprint(HHTut.simConfig.__dict__)
+pprint(sim.specs.netParams.__dict__)
+pprint(HHTut.netParams.__dict__)
+pprint(sim.__dict__)
+
+import json
+
+def save_json(obj, filename):
+    with open(filename, "w") as f:
+        json.dump(obj, f, indent=2, default=str)
+
+save_json(sim.specs.simConfig.__dict__, "sim_specs_simConfig.json")
+save_json(HHTut.simConfig.__dict__, "HHTut_simConfig.json")
+save_json(sim.specs.netParams.__dict__, "sim_specs_netParams.json")
+save_json(HHTut.netParams.__dict__, "HHTut_netParams.json")
+save_json(sim.__dict__, "sim_dict.json")
